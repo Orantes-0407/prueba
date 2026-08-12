2d97513b73b9f0bc8d80c972179f37f7de695e45
Melany Rosmery Pérez Orantes <mperezo37@miumg.edu.gt>
2026-08-12 09:24:06 -0600

    Add author and license to manifest file

--- stock_analytic_replenishment/__manifest__.py
+++ stock_analytic_replenishment/__manifest__.py
@@ -3,9 +3,12 @@
     'version': '1.0',
     'category': 'Inventory',
     'summary': 'Propaga la analítica a las órdenes de compra desde reglas de reordenamiento',
+    'author': 'Bitsis',
+    'license': 'LGPL-3',
     'depends': ['stock', 'purchase', 'purchase_stock', 'analytic'],
     'data': [
         'views/purchase_order_views.xml',
+        'views/stock_warehouse_orderpoint_views.xml',
     ],
     'installable': True,
     'application': False,
